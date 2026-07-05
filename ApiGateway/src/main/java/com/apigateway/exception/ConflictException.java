91553ad3858f4344719dc6bd4cedb85cad011b51
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-07-05 15:51:23 +0530

    Delete ApiGateway directory

--- ApiGateway/src/main/java/com/apigateway/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.apigateway.exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(String message){
-        super(message);
-    }
-}
--- ApiGateway/src/main/java/com/apigateway/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.apigateway.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
