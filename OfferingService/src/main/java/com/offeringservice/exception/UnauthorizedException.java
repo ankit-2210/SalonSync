d971d114480e9f18ba8dc5d91618d8ea8eab72df
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-07-05 15:52:58 +0530

    Delete OfferingService directory

--- OfferingService/src/main/java/com/offeringservice/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.offeringservice.exception;
-
-public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException(String message){
-        super(message);
-    }
-}
-
